10d5d96dcc73d34500f5a4aa4a67827cd1c5f2b6
Eebad Reza <95500079+eebadreza@users.noreply.github.com>
2024-11-03 02:11:27 +0530

    Initial commit

---
+++ src/js/helper.js
@@ -0,0 +1,81 @@
+// import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from "./config";
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+      setTimeout(function () {
+        reject(new Error(`Request took too long! Timeout after ${s} second`));
+      }, s * 1000);
+    });
+};
+
+
+export const AJAX = async function(url, uploadData = undefined){
+  try {
+  
+  const fetchPro = uploadData ? await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(uploadData)
+  }) : await fetch(url);
+
+
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+    // console.log(data);
+    if (!res.ok) 
+        throw new Error(`${data.message} ${res.status}`)
+
+    return data;
+} 
+catch (error) {
+    // alert(error);
+    throw error;
+}
+}
+  
+
+// export const getJSON = async function(url){
+    
+//     try {
+//         const fetchPro =await fetch(url)
+//         const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+//         const data = await res.json();
+//         // console.log(data);
+//         if (!res.ok) 
+//             throw new Error(`${data.message} ${res.status}`)
+    
+//         return data;
+//     } 
+//     catch (error) {
+//         // alert(error);
+//         throw error;
+//     }
+// }
+
+// export const setJSON = async function(url, uploadData){
+//   try {
+//       const fetchPro =await fetch(url, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(uploadData)
+//       })
+
+//       const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]);
+//       const data = await res.json();
+//       // console.log(data);
+      
+//       if (!res.ok) 
+//           throw new Error(`${data.message} ${res.status}`)
+  
+//       return data;
+//   } 
+//   catch (error) {
+//       // alert(error);
+//       throw error;
+//   }
+// }
